97433a8afd5f641d29e9c0deec74a72549bdaa40
Alexander Lam <alam@proteus.com>
2018-03-12 11:52:58 -0700

    Added routes 2 and 3 of API

--- bbb_app.py
+++ bbb_app.py
@@ -24,13 +24,60 @@ session = Session(engine)
 # Flask App
 app = Flask(__name__)
 
+# Utility
+def parse_sample(sample_id):
+    if type(sample_id) == str
+        id = re.search('\d+',sample_id)
+        if id == None:
+            print('No ID found.')
+            return None
+        else:
+            id = int(id.group(0))
+            return id
+    elif type(sample_id) == int:
+        return sample_id
+    else:
+        print('Unexpected input.')
+        return None
+
 # Create API routes
 # 1- Dashboard homepage
+@app.route('/')
+def home_template():
+    return render_template('index.html')
+
 # 2- List of sample names (list format)
+@app.route('/names')
+def sample_names():
+    columns = Base.metadata.tables['samples'].columns.keys()
+    # Confirm sample_ids adhere to format "BB_xxxx"
+    samples = []
+    for candidate in columns:
+        if candidate[:3].lower() == "bb_":
+            samples.append(candidate)
+    return jsonify(samples)
+
+
 # 3- List of otu descriptions (list format)
+@app.route('/otu')
+def otu_desc():
+    return jsonify([x[0] for x in session.query(otu.lowest_taxonomic_unit_found).all()])
+
 # 4- metadata object for given sample
+@app.route('/metadata/<sample>')
+def metadata(sample):
+    pass
+
 # 5- washing frequency as a singular number
+@app.route('/wfreq/<sample>')
+def washing_freq(sample):
+    pass
+
 # 6- List of distionaries containing sorted lists for otu_ids and sample_values
+@app.route('/samples/<sample>')
+def sample_data(sample):
+    pass
+
 
 
 
